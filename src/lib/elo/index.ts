9d2339143fb1d5184bb8624156635affb77971d3
Baptiste-lg <baptiste.le-glaunec@epitech.eu>
2026-09-17 14:25:09 +0200

    [ADD] add Elo module public API index

---
+++ src/lib/elo/index.ts
@@ -0,0 +1,3 @@
+export { calculateElo, expectedScore, getKFactor } from './calculator';
+export { DEFAULT_ELO, K_FACTOR_NEW, K_FACTOR_MID, K_FACTOR_EST } from './types';
+export type { EloInput, EloResult, EloRating } from './types';
